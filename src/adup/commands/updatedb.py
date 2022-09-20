f71d12940da22534698e27285097312d390de156
Olivier ORABONA <oorabona@users.noreply.github.com>
2022-09-21 00:39:35 +0200

    major refact using scandir instead of glob

--- src/adup/commands/updatedb.py
+++ src/adup/commands/updatedb.py
@@ -1,5 +1,4 @@
 import errno
-import glob
 import os
 import sys
 
@@ -8,7 +7,7 @@ from alive_progress import alive_bar
 
 from adup.backends import refreshdb, updatedb
 from adup.cli import cli
-from adup.utils import debug, getEngine
+from adup.utils import debug, get_multi_value_option, getEngine
 
 
 @click.group()
@@ -21,7 +20,7 @@ def cli():  # noqa: F811
     "-i",
     "--include",
     "include",
-    default=[f"{os.path.curdir}"],
+    default=[],
     show_default=True,
     help="Add this path to the list.",
     type=click.Path(exists=True, file_okay=False, resolve_path=True),
@@ -71,62 +70,72 @@ def cli(ctx, include, exclude, verbose, refresh, progress):
     getEngine(ctx.config)
 
     # Get the list of all the paths to check
-    pathsSection = ctx.config.items("paths")
+    pathsSection = ctx.config["paths"]
 
-    # Add the paths from the command line
-    paths = []
-    paths.extend(include)
-
-    # Add the paths from the command line
-    if "include" in pathsSection:
-        for path in pathsSection.include:
-            paths.extend(path)
+    # Get the list of paths to include from the config file
+    # and merge with the list of paths to include from the command line
+    include_path = list(include) + get_multi_value_option(pathsSection, "include[]")
 
     # Remove empty paths and duplicates
-    paths = list({path.strip() for path in paths if path})
-    debug(f"Included Paths: {paths}")
+    include_paths = list({os.path.abspath(path.strip()) for path in include_path if path})
+    debug(f"Included Paths: {include_paths}")
 
-    # Remove the paths excluded from the command line
-    for path in exclude:
-        if path in paths:
-            paths.remove(path)
+    # Get the list of paths to exclude from the config file
+    # and merge with the list of paths to exclude from the command line
+    exclude_path = list(exclude) + get_multi_value_option(pathsSection, "exclude[]")
 
-    # Remove the paths excluded from the config file
-    if "exclude" in pathsSection:
-        for path in pathsSection.exclude:
-            if path in paths:
-                paths.remove(path)
+    # Remove empty paths and duplicates
+    exclude_paths = list({os.path.abspath(path.strip()) for path in exclude_path if path})
+    debug(f"Excluded Paths: {exclude_paths}")
+
+    # Remove excluded paths from included paths
+    paths = [path for path in include_paths if path not in exclude_paths]
 
     debug(f"After Excluded Paths: {paths}")
     if verbose:
         click.secho("Updating information from paths: %s" % ", ".join(paths), fg="green")
 
-    with alive_bar(0, bar="blocks", spinner="dots_waves", dual_line=True, disable=not progress) as bar:
+    with alive_bar(manual=True, bar="blocks", spinner="dots_waves", dual_line=True, disable=not progress) as bar:
+        current = 0
         for path in paths:
-            for filename in glob.iglob(f"{path}**/**", recursive=True):
-                if verbose:
-                    click.secho("Updating information for : %s" % filename, fg="green")
-
-                try:
-                    stat = os.stat(filename)
-
-                    # Check if the file is a regular file
-                    if not stat.st_mode & 0o100000:
-                        if verbose:
-                            click.secho("File %s is not a regular file, skipping" % filename, fg="red")
+            current += 1
+            for entry in os.scandir(path):
+                if entry.is_dir(follow_symlinks=False):
+                    if entry.name.startswith(".") or entry.path in exclude_paths:
                         continue
-
-                    updatedb(os.path.dirname(filename), os.path.basename(filename), stat)
-                    bar()
-                except OSError as e:  # pragma: no cover
-                    if e.errno == errno.ENOENT:
-                        click.secho("File %s does not exist, skipping" % filename, fg="red")
-                        continue
-                    else:
-                        raise
-                except Exception as e:  # pragma: no cover
-                    click.secho("Error while updating {}: {}".format(filename, e), fg="red")
+                    paths.append(entry.path)
                     continue
+                if entry.is_file(follow_symlinks=False):
+                    filename = entry.path
+
+                    if verbose:
+                        click.secho("Updating information for : %s" % filename, fg="green")
+
+                    try:
+                        stat = entry.stat(follow_symlinks=False)
+                        bar.text(f"Updating information for : {filename}")
+
+                        # Check if the file is a regular file
+                        if not stat.st_mode & 0o100000:
+                            if verbose:
+                                click.secho("File %s is not a regular file, skipping" % filename, fg="red")
+                            continue
+
+                        updatedb(os.path.dirname(filename), os.path.basename(filename), stat)
+
+                        # Update progress bar only if progress is made
+                        progress = current / len(paths)
+                        if bar.current() < progress:
+                            bar(progress)
+                    except OSError as e:  # pragma: no cover
+                        if e.errno == errno.ENOENT:
+                            click.secho("File %s does not exist, skipping" % filename, fg="red")
+                            continue
+                        else:
+                            raise
+                    except Exception as e:  # pragma: no cover
+                        click.secho("Error while updating {}: {}".format(filename, e), fg="red")
+                        continue
 
     if refresh:
         if verbose:
